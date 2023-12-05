f391cb1dac7aa65be9737fdee9ac7358e5fb8adc
Marco Seraphin <seraphin@develope-it.de>
2023-12-05 15:02:20 +0100

    Fix BorderlessEntry styling

--- MAUIHandlerSample/MauiProgram.cs
+++ MAUIHandlerSample/MauiProgram.cs
@@ -26,6 +26,20 @@ public static class MauiProgram
 		builder.Logging.AddDebug();
 #endif
 
+		Microsoft.Maui.Handlers.EntryHandler.Mapper.AppendToMapping(nameof(BorderlessEntry), (handler, view) =>
+		{
+			if (view is BorderlessEntry)
+			{
+#if __ANDROID__
+        handler.PlatformView.SetBackgroundColor(Android.Graphics.Color.Transparent);
+		// handler.PlatformView.BackgroundTintList = Android.Content.Res.ColorStateList.ValueOf(Colors.Transparent.ToNative());
+#elif __IOS__
+				handler.PlatformView.BackgroundColor = UIKit.UIColor.Clear;
+				handler.PlatformView.BorderStyle = UIKit.UITextBorderStyle.None;
+#endif
+			}
+		});
+
 		return builder.Build();
 	}
 }
